639411a29e2d0c5bf39a5d02bdd6f8de8099b126
Gaurav <gauravkr7488@gmail.com>
2026-02-04 16:53:15 +0530

    added magnify button in the home page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, IconButton } from "react-native-paper";
 
 export default function RootLayout() {
+  const router = useRouter();
   return (
     <SafeAreaProvider>
       <PaperProvider>
         <Stack>
-          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Home",
+              headerRight: () => (
+                <IconButton
+                  icon="magnify"
+                  onPress={() => router.push("./SearchPage")}
+                ></IconButton>
+              ),
+            }}
+          />
         </Stack>
       </PaperProvider>
     </SafeAreaProvider>
