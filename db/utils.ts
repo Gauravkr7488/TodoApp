aa23aa92e288aba8221da4807fac5919825ca5f3
Gaurav <gauravkr7488@gmail.com>
2026-02-08 11:57:54 +0530

    adjusted the util fun

--- db/utils.ts
+++ db/utils.ts
@@ -1,7 +1,7 @@
 /**
  * @description for a comma seperated strings only 
  */
-export function stringToArray(s: string): string[] {
+export function stringToArray(s: string): any[] {
   if (!s) return []; // handle empty string
   return s.split(",").map(item => item.trim());
 }
