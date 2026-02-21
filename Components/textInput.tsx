b4d555621893681f7dd256025f05b7c635f063d5
Gaurav <gauravkr7488@gmail.com>
2026-02-22 04:14:54 +0530

    new components

---
+++ Components/textInput.tsx
@@ -0,0 +1,25 @@
+import { useTheme } from "@/Components/ThemeContext";
+import { darkThemeColors, lightThemeColors } from "@/Constants/Colours";
+import React from "react";
+import { TextInput, TextInputProps } from "react-native-paper";
+type Props = TextInputProps;
+
+export default function CustomTextInput({ style, ...rest }: Props) {
+  const { isDark } = useTheme();
+  const theme = isDark ? darkThemeColors : lightThemeColors;
+
+  return (
+    <TextInput
+      {...rest}
+      placeholderTextColor={theme.textSecondary}
+      style={[
+        {
+          backgroundColor: theme.surface,
+          color: theme.textPrimary,
+          borderColor: theme.border,
+        },
+        style,
+      ]}
+    />
+  );
+}
