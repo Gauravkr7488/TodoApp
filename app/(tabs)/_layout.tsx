43401972f51b9a72b26a3d4456bc9d0b3bf84b17
Gaurav <gauravkr7488@gmail.com>
2026-02-22 00:03:19 +0530

    applied theme in layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,39 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "@/Components/ThemeContext";
+import { darkThemeColors, lightThemeColors } from "@/Constants/Colours";
 
 export default function TabsLayout() {
+  const { isDark } = useTheme();
+  const theme = isDark ? darkThemeColors : lightThemeColors;
   return (
     <Tabs
       screenOptions={{
+        headerStyle: {
+          backgroundColor: theme.background,
+        },
+        headerTintColor: theme.textPrimary,
         tabBarShowLabel: false, // hide text
         tabBarStyle: {
           height: 100, // increase from default (usually ~50)
           paddingBottom: 10, // optional, adjusts icon spacing
+          backgroundColor: theme.background,
         },
         tabBarIconStyle: {
           height: 60,
         },
+        // tabBarInactiveTintColor: "#9CA3AF",
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
+          title: "Home",
           tabBarIcon: ({ color, size, focused }) => (
             <Ionicons
               name={focused ? "home" : "home-outline"}
               size={size}
-              color={color}
+              color={theme.accent}
             />
           ),
         }}
@@ -35,7 +46,7 @@ export default function TabsLayout() {
             <Ionicons
               name={focused ? "list" : "list-outline"}
               size={size}
-              color={color}
+              color={theme.accent}
             />
           ),
         }}
@@ -48,7 +59,7 @@ export default function TabsLayout() {
             <Ionicons
               name={focused ? "settings" : "settings-outline"}
               size={size}
-              color={color}
+              color={theme.accent}
             />
           ),
         }}
