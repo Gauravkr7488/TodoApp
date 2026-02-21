64038b74242bd62b7c64e5b46103ba04ca8b7527
Gaurav <gauravkr7488@gmail.com>
2026-02-21 13:16:43 +0530

    fixed the root layout with tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,13 @@
-import { Stack, useRouter } from "expo-router";
+import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { PaperProvider, IconButton, MD3LightTheme } from "react-native-paper";
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
 
 export default function RootLayout() {
-  const router = useRouter();
   return (
     <SafeAreaProvider>
-      <PaperProvider 
-      theme={MD3LightTheme}
-      >
+      <PaperProvider theme={MD3LightTheme}>
         <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Home",
-              headerRight: () => (
-                <IconButton
-                  icon="magnify"
-                  onPress={() => router.push("./SearchPage")}
-                ></IconButton>
-              ),
-            }}
-          />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
       </PaperProvider>
     </SafeAreaProvider>
