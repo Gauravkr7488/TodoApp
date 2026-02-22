d6080940af803d4a3592014774c5e416de7fb601
Gaurav <gauravkr7488@gmail.com>
2026-02-23 02:10:36 +0530

    FFFFF

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,8 @@
 import { getDefaultConfig } from '@react-native/metro-config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default getDefaultConfig(__dirname);
